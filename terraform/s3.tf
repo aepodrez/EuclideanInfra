4881fd901141b06cab8e7ae625b0d2acf7a75a81
Alex Podrez <aepodrez@mit.edu>
2026-02-27 23:25:03 +0000

    Allow CloudWatch Logs export tasks to write into pipeline S3 bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -30,6 +30,72 @@ resource "aws_s3_bucket_public_access_block" "pipeline_data" {
   restrict_public_buckets = true
 }
 
+data "aws_iam_policy_document" "pipeline_data_bucket_policy" {
+  statement {
+    sid    = "AllowCloudWatchLogsExportGetBucketAcl"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["logs.${data.aws_region.current.name}.amazonaws.com"]
+    }
+
+    actions   = ["s3:GetBucketAcl"]
+    resources = [aws_s3_bucket.pipeline_data.arn]
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [data.aws_caller_identity.current.account_id]
+    }
+
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"]
+    }
+  }
+
+  statement {
+    sid    = "AllowCloudWatchLogsExportPutObject"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["logs.${data.aws_region.current.name}.amazonaws.com"]
+    }
+
+    actions = ["s3:PutObject"]
+    resources = [
+      "${aws_s3_bucket.pipeline_data.arn}/manual-exports/*",
+      "${aws_s3_bucket.pipeline_data.arn}/cloudwatch-logs/*"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [data.aws_caller_identity.current.account_id]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "pipeline_data" {
+  bucket = aws_s3_bucket.pipeline_data.id
+  policy = data.aws_iam_policy_document.pipeline_data_bucket_policy.json
+}
+
 # Create folder prefixes as S3 objects
 resource "aws_s3_object" "universe_folder" {
   bucket  = aws_s3_bucket.pipeline_data.id
